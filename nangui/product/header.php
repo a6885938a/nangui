bd0b3ca6ecfebf74acdd96d7ff4cd9059c953e4c
a6885938a <332178065@qq.com>
2016-05-23 17:19:11 +0800

    1

--- nangui/product/header.php
+++ nangui/product/header.php
@@ -3,7 +3,7 @@
         <div class="container">
           <div class="navbar-header" >
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-   <div class="navbar-brand-wrap"> <a class="navbar-brand" href="/index.html"> <img src="../images/logo.png" alt=""> </a> </div>
+   <div class="navbar-brand-wrap"> <a class="navbar-brand" href="../index.html"> <img src="../images/logo.png" alt=""> </a> </div>
           </div>
           <!-- .navbar-header -->
           <div class="collapse navbar-collapse" id="navbar-collapse" >
