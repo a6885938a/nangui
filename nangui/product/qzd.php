b08908737c972f0ab22b0f7b760c2bfef7afdf3c
a6885938a <332178065@qq.com>
2016-06-06 23:56:54 +0800

    1

--- nangui/product/qzd.php
+++ nangui/product/qzd.php
@@ -9,13 +9,13 @@
 <meta name="author" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <?php
-include dirname(__FILE__).'/shop-li-header.php'; 
-?>
+  include dirname(__FILE__).'/shop-li-header.php'; 
+  ?>
 </head>
 <body>
 <?php
-include dirname(__FILE__).'/header.php'; 
-?>
+  include dirname(__FILE__).'/header.php'; 
+  ?>
 <div class="b-wrap">
   <div class="container li-wrap" >
     <div class="row clearfix">
@@ -23,7 +23,6 @@ include dirname(__FILE__).'/header.php';
         <ul class="breadcrumb">
           <li> <a href="../index.html">首页</a> </li>
           <li> <a href="../index.html#about">优势产品</a> </li>
-         
           <div class="qrcode  visible-md visible-lg ">
             <div class="mob-s" >手机扫码分享</div>
             <div id="output"></div>
@@ -48,8 +47,8 @@ include dirname(__FILE__).'/header.php';
       </div>
       <div class="row">
         <?php
-include dirname(__FILE__).'/shop-group-li.php'; 
-?>
+  include dirname(__FILE__).'/shop-group-li.php'; 
+  ?>
         <div class="col-md-9 column">
           <div class="proWrapper ">
             <div class="supply">
@@ -59,29 +58,22 @@ include dirname(__FILE__).'/shop-group-li.php';
               <div class="supplyContent" >
                 <div id="myTab1Content" class="tab-content">
                   <div class="tab-pane fade in active">
-                    <p> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_000.jpg"> 
-                    <br/>   <br/>
-                    <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_0000.jpg"><p style=" padding:0 8px; ">
-产品特点：节省人工、生产高效、定位精确、安全可靠
-本机专用于铝型材氧化、电泳、喷涂、着色等，适用于温度高，带有腐蚀性、酸性气体等的环境中。整机的钢结构件表面经抛丸打砂处理，涂环氧富锌底漆、云铁中间漆、和防腐面漆。大车采用集中驱动、变频电机（带制动），双输出，走齿条，可选择固定钩或带开合机构（采用螺纹丝杆，锡青铜螺母，3个够，每次吊3排料）。由电脑系统集中控制，可以保存多套程序，可选择手动、半自动和全自动功能。</p><p style=" padding:0 8px;">全自动氧化机图纸  ： </p> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/qzd/2633565630_238983798.jpg">     <br/>   <br/>
- <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_0001.jpg">
-
- <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/qzd/2643037900_238983798.jpg">
-   <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_003.jpg">  <br/>   <br/> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_004.jpg"> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_005.jpg"> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_006.jpg">
-  <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_0002.jpg">
-  <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_007.jpg">
-    <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_0003.jpg">
-                    <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_008.jpg">
-                       <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_0004.jpg">
-                    <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_009.jpg"> 
-                       <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_0005.jpg">
-                     <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_010.jpg"> </p>
-                     <p style="padding:0 8px;">
-                   欢迎各界新老顾客使用南桂起重机<br/>
-联系方式<br/>
-销售热线：<a style="color:#313131;"href="tel:0757-87655338">0757-87655338</a><br/>
-                     
-                     </p>
+                    <p> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_000.jpg"> <br/>
+                      <br/>
+                      <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_0000.jpg">
+                    <p style=" padding:0 8px; "> 产品特点：节省人工、生产高效、定位精确、安全可靠
+                      本机专用于铝型材氧化、电泳、喷涂、着色等，适用于温度高，带有腐蚀性、酸性气体等的环境中。整机的钢结构件表面经抛丸打砂处理，涂环氧富锌底漆、云铁中间漆、和防腐面漆。大车采用集中驱动、变频电机（带制动），双输出，走齿条，可选择固定钩或带开合机构（采用螺纹丝杆，锡青铜螺母，3个够，每次吊3排料）。由电脑系统集中控制，可以保存多套程序，可选择手动、半自动和全自动功能。</p>
+                    <p style=" padding:0 8px;">全自动氧化机图纸  ： </p>
+                    <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/qzd/2633565630_238983798.jpg"> <br/>
+                    <br/>
+                    <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_0001.jpg"> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/qzd/2643037900_238983798.jpg"> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_003.jpg"> <br/>
+                    <br/>
+                    <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_004.jpg"> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_005.jpg"> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_006.jpg"> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_0002.jpg"> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_007.jpg"> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_0003.jpg"> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_008.jpg"> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_0004.jpg"> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_009.jpg"> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_0005.jpg"> <img class="lazy" src="../images/loading.jpg" data-original="../images/nh_product/public/20160427_235226_010.jpg">
+                    </p>
+                    <p style="padding:0 8px;"> 欢迎各界新老顾客使用南桂起重机<br/>
+                      联系方式<br/>
+                      销售热线：<a style="color:#313131;"href="tel:0757-87655338">0757-87655338</a><br/>
+                    </p>
                   </div>
                 </div>
                 <div class="cl" ></div>
@@ -94,10 +86,10 @@ include dirname(__FILE__).'/shop-group-li.php';
   </div>
 </div>
 <?php
-include dirname(__FILE__).'/shop-li-footer.php'; 
-?>
+  include dirname(__FILE__).'/shop-li-footer.php'; 
+  ?>
 <?php
-include dirname(__FILE__).'/delayload.php'; 
-?>
+  include dirname(__FILE__).'/delayload.php'; 
+  ?>
 </body>
 </html>
--- 素材/Polycom25周年_files/loading1_71b35bb.js
+++
@@ -1,1 +0,0 @@
-var imageLoad=new ImageLoad,imageUrls=[],$loading=$("#loading");imageLoad.queueImage(imageUrls).queueImage(loadImgArr).imageLoadingProgressCallback(function(a){var a=Math.floor(a);$loading.find(".progress span").html(a+"%"),$loading.find(".progress_bar span").css({width:a+"%"})},function(){(new Main).init().loading()});
