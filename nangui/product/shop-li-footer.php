bbcc2a2d58c518f231b0c4b6711d8f56f585a13b
a6885938a <332178065@qq.com>
2016-06-08 18:06:57 +0800

    1

--- nangui/product/shop-li-footer.php
+++ nangui/product/shop-li-footer.php
@@ -9,7 +9,7 @@
   <div id="rocket-lit" style="display: block;"></div>
 </div>
 <script src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script> 
-<script type="text/javascript" src='../js/bootstrap.min.js'></script> 
+<script src='http://apps.bdimg.com/libs/bootstrap/3.3.4/js/bootstrap.min.js'></script> 
 <script src='../js/totop.js'></script> <!--回到顶部-->
 <script src="../js/jquery.qrcode.min.js"></script><!--二维码--> 
 <script src="../js/stickUp.min.js"></script> 
--- nangui/product/shop-li-header.php
+++ nangui/product/shop-li-header.php
@@ -1,6 +1,6 @@
 <link rel="icon" href="../assets/img/favicon.ico">
-<link rel='stylesheet' href='../css/bootstrap.min.css'>
+<link href="http://t1.mcp.cn/oshpgkml/644502/nangui/css/bootstrap.min.css" rel="stylesheet">
 <link rel='stylesheet' href='../css/vendor.css'>
 <link rel='stylesheet' href='../css/style.css'>
 <link rel='stylesheet' href='../css/product.css'>
-<script src='../js/html5shiv.min.js'></script>
+<script src='http://apps.bdimg.com/libs/html5shiv/3.7/html5shiv.min.js'></script><!--IE9以下版本浏览器对HTML5新增标签不识别-->
